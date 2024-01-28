c73925f4d31c10cff8ab79604a1e760fc5c88c35
User <user@email.com>
2024-01-28 17:38:48 +0700

    Fix script URL

--- nvim.sh
+++ nvim.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 
-GITHUB_WORKSPACE=https://github.com/XanMarta/workspace.git
+SCRIPT_URL="https://raw.githubusercontent.com/XanMarta/workspace/master/scripts/nvim.sh"
 
-# Clone workspace
-echo "----- Cloning workspace -----"
-mkdir -p ~/.temp
-rm -rf ~/.temp/workspace
-git clone $GITHUB_WORKSPACE ~/.temp/workspace
-
-# Backup old configuration
-echo "----- Backing up old configuration -----"
-if [ -d ~/.config/nvim ]; then
-  rm -rf ~/.config/nvim.bk/
-  mv ~/.config/nvim ~/.config/nvim.bk
-fi
-
-# Copy new configuration
-echo "----- Copying new configuration files -----"
-cp -r ~/.temp/workspace/lazyvim/nvim ~/.config/nvim
-
-# Remove workspace
-echo "----- Removing workspace -----"
-rm -rf ~/.temp/workspace
+curl -s $SCRIPT_URL | bash
 
